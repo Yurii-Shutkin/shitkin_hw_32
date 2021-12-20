e2ccd2737bbcdc4f1e3953e6d6c16ae2e2599375
Yurii-Shutkin <yurashutkinn@gmail.com>
2021-12-21 00:32:02 +0200

    init 3-rd task

--- js/solution.js
+++ js/solution.js
@@ -22,10 +22,13 @@
     ul.removeAttribute('data-dog-tail');
 
     // Task 2
+
+    const numArr = [1,2, [1.1,1.2,1.3] ,3];
+    const list = generateList(numArr); 
     
-    const generateList = function(arr) {
+    function generateList(array) {
         const ul = document.createElement('ul');
-        arr.forEach(item => { 
+        array.forEach(item => { 
             if(Array.isArray(item)) {
                 ul.innerHTML += `<li>${generateList(item).outerHTML}</li>`;
             } else {
@@ -34,9 +37,31 @@
         }) 
         return ul;
     }
-
-    const array = [1,2, [1.1,1.2,1.3] ,3];
-    const list = generateList(array); 
+    
+    console.log(generateList(numArr));
     ul.after(list);
+
+    // Task 3
+
+    const table = newTable();
+    
+    function newTable() {
+        const table = document.createElement('table');
+        for(let i = 1; i <= 10; i++) {
+            const tr = document.createElement('tr');
+            table.appendChild(tr);
+            for(let j = 1; j <= 10; j++) {
+                const td = document.createElement('td');
+                tr.appendChild(td); 
+                for(let k = 1; k <= 100; k++) {
+                    tr.appendChild(td).innerHTML = k; 
+                }
+            } 
+        }
+        return table;
+    }
+
+    list.after(table)
+    console.log(newTable())
 
 })();
