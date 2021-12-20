fc421975dd003750664011e7adefb8a9d5e92969
Yurii-Shutkin <yurashutkinn@gmail.com>
2021-12-20 23:16:10 +0200

    complete task2

--- js/solution.js
+++ js/solution.js
@@ -21,4 +21,22 @@
     ul.firstElementChild.setAttribute('data-my-name', 'Yurii');
     ul.removeAttribute('data-dog-tail');
 
+    // Task 2
+    
+    const generateList = function(arr) {
+        const ul = document.createElement('ul');
+        arr.forEach(item => { 
+            if(Array.isArray(item)) {
+                ul.innerHTML += `<li>${generateList(item).outerHTML}</li>`;
+            } else {
+                ul.innerHTML += `<li>${item}</li>`;
+            }
+        }) 
+        return ul;
+    }
+
+    const array = [1,2, [1.1,1.2,1.3] ,3];
+    const list = generateList(array); 
+    ul.after(list);
+
 })();
